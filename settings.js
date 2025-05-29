8f77a972632f39a5957930dd92ef1ca11ba24a7f
Ayannx <72630907+ayaninza@users.noreply.github.com>
2025-05-29 06:22:52 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Alya',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Alya",
+  botOwner: 'Alya', // Your name
+  ownerNumber: '923148368989', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.1",
 };
